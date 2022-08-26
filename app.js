f120e81f0b7af2e5b0d8250648767d579c197aee
Masum Billah <masum.monpura@gmail.com>
2022-08-26 18:10:41 +0600

    add questions

---
+++ app.js
@@ -0,0 +1,21 @@
+//Problem: 1
+
+const name = "masum";
+let age = 30;
+
+//Problem: 2
+//a
+let str = `I am ${name} and my age is ${age}`;
+
+console.log(str);
+//b
+const obj = {
+  name: "masum",
+  age: 30,
+  education: "masters",
+  job: "govt",
+};
+
+const objStr = `I am ${obj.name} and my age is ${obj.age}`;
+
+console.log(objStr)
