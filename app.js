73f7466582267f9a1865a4c00e6b8aa243ed9315
Masum Billah <masum.monpura@gmail.com>
2022-08-26 20:05:58 +0600

    problem 7 solved

--- app.js
+++ app.js
@@ -41,9 +41,34 @@ const threePara = (a, b, c) => a * b * c;
 console.log(threePara(3, 4, 5));
 //3.4
 
-const twoPara = (a,b) => {
-    const addA = a + 2;
-    const addB = b + 2;
-    return addA * addB;
-}
-console.log(twoPara(3,5))
+const twoPara = (a, b) => {
+  const addA = a + 2;
+  const addB = b + 2;
+  return addA * addB;
+};
+console.log(twoPara(3, 5));
+
+//Problem: 5
+
+const arr = [2, 3, 5, 8, 9];
+const multipliedArray = arr.map((i) => i * 5);
+console.log(multipliedArray);
+
+//Problem: 6
+
+const nums = [2, 15, 41, 52, 21, 44, 12, 99];
+const oddNum = nums.filter((num) => num % 2 === 1);
+console.log(oddNum)
+
+//Problem: 7
+
+const objArray = [
+    {name: "a" , price: 12000},
+    {name: "b", price: 7000},
+    {name: "c", price: 5000},
+    {name: "d", price: 8000 }
+]
+
+const selectedObj = objArray.filter(elem => elem.price === 5000);
+
+console.log(selectedObj)
